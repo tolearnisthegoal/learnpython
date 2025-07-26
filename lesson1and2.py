52adaf69b2121cf0a475d3621aefe2636c55a45b
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-07-26 14:05:53 +0500

    challenges lesson1and2.py copleted

--- lesson1and2.py
+++ lesson1and2.py
@@ -70,3 +70,57 @@ This is Day 2
 I'm learning comments
 And print formatting!'''
 
+# Print this sentence:
+# He said, "Python is awesome!"
+
+print("\"Python is awesome!\"")
+
+# Print:
+# "This" is in quotes, using escape characters.
+print("\"This\" is in quotes")
+
+# Print: 1--2--3 using print() 
+print(1,2,3, sep="--") #  it's not like this print("1 , 2 , 3", sep="--")
+
+# Print: apple**banana**cherry using sep="**"
+print("apple", "banana", "cherry", sep="**") #not like this print("apple, banana, cherry", sep="**")
+
+# Print: hi!hello!hey! using sep="!" and multiple values.
+print("hi","hello","hey", sep="!")
+
+# Print "Begin" and "End" on the same line using end="".
+print("begin", "end")
+print("begin", end="end")
+
+# just adding a new line because last one was getting attached to the next one.
+print()
+
+# Print three print() statements, but keep them on one line using end=" ".
+print("one ", end=" ")
+print("two ", end=" ")
+print("three ", end="\n")
+
+# Print: X--Y--Z using sep="--" and add end="DONE" at the end.
+print("X","Y","Z ", sep="--", end="end ")
+
+
+# just adding a new line because last one was getting attached to the next one.
+print()
+
+# Print: "1", "hi", "2" using sep="**" and end=" :) ".
+print(1, "hi", 2, sep="**", end=" :) ")
+
+# just adding a new line because last one was getting attached to the next one.
+print()
+
+
+# Print this in one line of code:
+
+# less
+# Copy
+# Edit
+# Apple##Juice##Now...Done
+
+print("#less \n#Copy \n#Edit #Apple##Juice##Now...Done")
+print("#less", "#Copy","#Edit", "#Apple##Juice##Now...Done", sep="\n")
+print("#less", "#Copy","#Edit", "#Apple##Juice##Now...Done", sep='\n')
