bbef9e92a3a37e3f0774bc25e38f4721dc9423c2
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-07-26 12:53:03 +0500

    First Edit

---
+++ lesson1and2.py
@@ -0,0 +1,25 @@
+print("====================")
+print("||  Hello, I’m Ray  ||")
+print("====================")
+
+
+
+print("")
+print("**********")
+print("*        *")
+print("*        *")
+print("*        *")
+print("**********")
+
+
+# Print your name using print().
+print("Asad")
+
+# Print your name and your age in one print statement.
+print("Asad 22")
+
+# Print this sentence exactly:
+# I love Python!
+
+print("I love Python!")
+
