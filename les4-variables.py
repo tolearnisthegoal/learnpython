ad76721972c9b0ccdec7b0009290b273edfa5a1f
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-07-26 20:24:59 +0500

    les4 Variable Challenges

---
+++ les4-variables.py
@@ -0,0 +1,117 @@
+# Assign your age to a variable and print it
+age = 22
+print(age)
+
+# Create two integers. Add them and print the result
+f = 12
+g = 12
+print(f + g)
+
+# Create a variable is_sunny and assign it True. Print it.
+is_sunny = True
+
+# Store your name in a variable and print: Hello, my name is _____
+name = "Ahmad"
+print("Hello, my name is ", name)
+
+# Create 4 variables (int, float, string, bool) and print their types using type()
+a = 7
+b = True
+c = "Hello"
+d = 7.55
+print( "a data type ", type(a), "b data type", type(b), "c data type", type(c), "d data type", type(d))
+
+# Create a variable called "snack" and give it any food.
+# Then change the value to another food and print both times.
+food = "apple"
+print(food)
+food = "banana"
+print(food)
+
+# Create 2 string variables: first_name and last_name
+# Combine them to print your full name
+first_name = "Ahmad"
+second_name = "Ali"
+print(first_name + " " + second_name)  #instead of coma you can use this also
+
+# Set variable x = 10
+# Print x
+# Set x = x + 5
+# Print x again
+
+x = 10
+print(x)
+x = x + 5
+print(x)
+
+# Assign 3.14 to a variable called "pi" and print its type
+pi = 3.14
+print(type(pi))
+
+# Create 2 numbers and check if the first is greater than the second. Store it in a variable and print it.
+h = 83
+g = 81
+k = h>g
+print(k)
+
+# Create a variable box = None
+# Print the variable and its type
+variablebox = None
+print(type(variablebox))
+
+# What will be the type of this: "3.14"
+# Write your guess in a comment and then check using type()
+# answer = str
+typecheck = "3.14"
+print(type(typecheck))
+
+# Try assigning 5 to a variable and then change it to a string like "Five". Print both times and their types.
+changingtypes = 5
+print(changingtypes, type(changingtypes))
+changingtypes = "Five"
+print(changingtypes, type(changingtypes))
+
+# Create at least one variable of each: int, float, str, bool, None
+# Print them all on separate lines
+integr = 12
+floatt = 12.20
+strr = "String"
+booool = True
+noon = None
+print(integr, floatt, strr, booool, noon, sep="\n")
+
+# Create a variable called "lucky_number" and set it to your favorite number. Print it.
+lucky_number = 11
+print(lucky_number)
+
+# Try creating a variable starting with a number like 1name = "Ali"
+# See what error you get and write it in a comment
+# 1name = "Ali"   
+# print(1name)      error is 1^name = "Ali" invalid decimal literal
+
+
+# Create a string variable and print it in uppercase using .upper()
+upper_case = "ali"
+upper = upper_case.upper()
+print(upper)
+
+# What type is this: True + 5
+# Guess first, then check using type()
+# answer = complex wrong answer, it's "int"
+typecheckagain = True + 5
+print(type(typecheckagain))
+
+# Assign "42" (as string) and 42 (as integer) to two variables
+# Print their types and try adding them — see what happens
+as_string  = "42"
+as_integer = 42
+# print(as_string + as_integer) # TypeError: can only concatenate str (not "int") to str
+
+# What will be the type and value of this:
+# aanswer = boolean 
+x = True + True + False
+print(x)
+print("type")
+print(type(x))
+
+
