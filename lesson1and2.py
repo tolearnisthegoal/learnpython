f894b27eecd8f9f215a602040947bc46bb9564c7
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-07-26 13:19:18 +0500

    Solved challenges from 4 to 9

--- lesson1and2.py
+++ lesson1and2.py
@@ -1,3 +1,13 @@
+# Add a multi-line comment anywhere in your code that explains what your code does.
+
+'''This is not a single program,
+but programs,
+am solving challenges
+as am learning
+'''
+
+
+
 print("====================")
 print("||  Hello, I’m Ray  ||")
 print("====================")
@@ -22,4 +32,41 @@ print("Asad 22")
 # I love Python!
 
 print("I love Python!")
+
+
+# Print your name on three lines using \n.
+print("name\nname\nname \n")
+
+# Print this in two lines using \n: Python Rocks
+print("Python \nRocks")
+
+# Add a comment above your code that says: # This prints my name.
+# This prints my name 
+
+# Print your favorite number with a comment after it:
+print(3) #My favorite number
+
+
+
+
+
+
+
+
+# Write a multi-line comment that says:
+
+# vbnet
+# Copy
+# Edit
+# This is Day 2
+# I'm learning comments
+# And print formatting!
+
+
+'''vbnet
+Copy
+Edit
+This is Day 2
+I'm learning comments
+And print formatting!'''
 
