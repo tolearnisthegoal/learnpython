6075eef81de280d8b806296793b7f134d5d94c86
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-07-27 13:40:55 +0500

    Complete challenges for operators

--- operators.py
+++ operators.py
@@ -25,3 +25,77 @@ print( l % m, "This is Modulus")
 n = 15
 o = 7
 print(l // o, "This is Floor division")
+
+# Add two numbers: 6 + 3
+print(6+3)
+
+# Subtract: 20 - 8
+print(20-8)
+
+# Multiply: 7 * 4
+print(7*4)
+
+# Divide: 24 / 6
+print(24 / 6)
+
+# Exponent: 2 ** 4
+print(2 ** 4)
+
+# Modulus (remainder): 17 % 5
+print(17 % 5)
+
+# Floor division: 17 // 5
+print(17 // 5)
+
+# What is the result of 3 + 5 * 2? Answe = 13
+print(3+5*2)
+
+# What will 10 - 3 ** 2 return? Answer = 1
+print(10 - 3 ** 2)
+
+# Predict 4 + 6 / 2 - 1 Answer = 10 Wrong correct is 6
+print( 4+6 / 2-1)
+
+# Create a variable x = 5, then increase it by 3 using +
+x = 5
+print(x + 3)
+
+# Create y = 10, then subtract 4 from it
+y = 10
+print(y - 4)
+
+# Multiply x and y and store it in z
+x = 2
+y = 4
+z = x*y
+print(z)
+
+# Use % to find if a number is even or odd (try with x)
+x = 90
+print(x%2, "If answer is zero the number is even")
+
+# Use // to divide z by 4
+z = 86
+print(z // 4)
+
+# Calculate area of a square (side = 6)
+side = 6
+print(side ** 2, "Area of square side")
+
+# Convert seconds into minutes using floor division (125 // 60)
+print(125 // 60)
+
+# Calculate compound interest using ** (e.g., 100 * 1.05 ** 2)
+print(100 * 1.05 ** 2)
+
+# Print “Python” 5 times using *
+iteration = "Python "
+print( iteration * 5)
+
+# Use + to join two strings: "Code" + "Lab"
+str1 = "Code"
+str2 = "Lab"
+print(str1 + str2)
+
+# What will True + True + False return? Answer = 2
+print(True + True + False)
