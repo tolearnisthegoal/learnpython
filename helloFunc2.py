a9fa9737625c989ac9988336f0643bd956ff480a
tolearnisthegoal <221804288+tolearnisthegoal@users.noreply.github.com>
2025-08-01 14:50:33 +0500

    learning

---
+++ helloFunc2.py
@@ -0,0 +1,9 @@
+def say_hello_to(name):
+    #print three greetings to the name provided
+    print('Good Morning ' + name)
+    print('Good Afternoon ' + name)
+    print('Good Evening '+ name)
+    
+    
+say_hello_to('ahmad')
+say_hello_to('Saira')
---
+++ hellofunc.py
@@ -0,0 +1,12 @@
+def hello():
+    #print three greetings
+    print('Good Morning')
+    print('Good Afternoon')
+    print('Good Evening')
+    
+hello()
+hello()
+print('One more time')
+hello()
+
+
